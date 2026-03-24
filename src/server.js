8d7a65161bea0afbbca9c9ec09db75b55b680ead
Roman Hrydin <romario198901@gmail.com>
2026-03-24 18:53:31 +0200

    complete the task

--- src/server.js
+++ src/server.js
@@ -1,55 +1,33 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
-import pino from 'pino-http';
 import 'dotenv/config';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { pino } from './middleware/logger.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
-app.use(cors());
-app.use(helmet());
-app.use(express.json());
+app.use(pino);
+
 app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
+  express.json({
+    type: ['application/json', 'application/vnd.api+json'],
   }),
 );
-app.get('/notes', (req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
-});
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
-});
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
-app.use((req, res) => {
-  res.status(404).json({
-    message: 'Route not found',
-  });
-});
 
-app.use((err, req, res, next) => {
-  const isProd = process.env.NODE_ENV === 'production';
+app.use(cors());
 
-  res.status(500).json({
-    message: isProd
-      ? 'Something went wrong, please try again'
-      : `${err.message}`,
-  });
-});
+app.use(helmet());
+app.use(notesRoutes);
+
+app.use(notFoundHandler);
+
+app.use(errorHandler);
+
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
