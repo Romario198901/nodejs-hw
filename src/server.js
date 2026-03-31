81f524f656dcd77cd00ff696a38499b8e9545e81
Roman Hrydin <romario198901@gmail.com>
2026-03-31 17:25:06 +0300

    complete the tasks

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { pino } from './middleware/logger.js';
 import notesRoutes from './routes/notesRoutes.js';
+import { errors } from 'celebrate';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -24,6 +25,8 @@ app.use(helmet());
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
+
+app.use(errors());
 
 app.use(errorHandler);
 
