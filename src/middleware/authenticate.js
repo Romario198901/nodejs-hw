9ab0b17df16bf825f32834e942da14caed8fc93f
Roman Hrydin <romario198901@gmail.com>
2026-04-07 20:36:52 +0300

    complete the task

---
+++ src/middleware/authenticate.js
@@ -0,0 +1,22 @@
+import createHttpError from 'http-errors';
+import { Session } from '../models/session.js';
+import { User } from '../models/user.js';
+
+export const authenticate = async (req, res, next) => {
+  const { accessToken } = req.cookies;
+  if (!accessToken) {
+    throw createHttpError(401, 'Missing access token');
+  }
+  const session = await Session.findOne({ accessToken });
+  if (!session) {
+    createHttpError(401, 'Session not found');
+  }
+  const isTokenExpired = new Date() > new Date(session.accessTokenValidUntil);
+  if (isTokenExpired) {
+    throw createHttpError(401, 'Access token expired');
+  }
+
+  const user = await User.findById(session.userId);
+  req.user = user;
+  next();
+};
---
+++ src/models/user.js
@@ -0,0 +1,34 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 8,
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
+userSchema.pre('save', function () {
+  if (!this.username) {
+    this.username = this.email;
+  }
+});
+userSchema.methods.toJSON = function () {
+  const object = this.toObject();
+  delete object.password;
+  return object;
+};
+
+export const User = model('User', userSchema);
